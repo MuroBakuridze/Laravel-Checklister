81fcf37c81f55a20a80a894c6b4de9ab7b600201
MuroBakuridze <muro.bakuridze@gmail.com>
2022-04-27 14:14:58 +0400

    Customised CoreUI interface and auth. blades

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,4 +1,183 @@
 <!doctype html>
+<html lang="en">
+ <head>
+ <!-- Required meta tags -->
+ <meta charset="utf-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+
+ <!-- CoreUI CSS -->
+ <link rel="stylesheet" href="https://unpkg.com/@coreui/coreui/dist/css/coreui.min.css" crossorigin="anonymous">
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery.perfect-scrollbar/1.5.5/css/perfect-scrollbar.css" integrity="sha512-2xznCEl5y5T5huJ2hCmwhvVtIGVF1j/aNUEJwi/BzpWPKEzsZPGpwnP1JrIMmjPpQaVicWOYVu8QvAIg9hwv9w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+ <title>{{ config('app.name', 'Laravel') }}</title>
+ </head>
+ <body class="c-app">
+
+   {{-- Sidebar --}}
+
+  <div class="sidebar sidebar-dark sidebar-fixed" id="sidebar">
+    <div class="sidebar-brand d-none d-md-flex">
+      <svg class="sidebar-brand-full" width="118" height="46" alt="CoreUI Logo">
+        <use xlink:href="assets/brand/coreui.svg#full"></use>
+      </svg>
+      <svg class="sidebar-brand-narrow" width="46" height="46" alt="CoreUI Logo">
+        <use xlink:href="assets/brand/coreui.svg#signet"></use>
+      </svg>
+    </div>
+      <ul class="sidebar-nav" data-coreui="navigation" data-simplebar="">
+        <li class="nav-item">
+          <a class="nav-link" href="index.html">
+            <svg class="nav-icon">
+              <use xlink:href="{{ asset('vendors/@coreui/icons/svg/free.svg#cil-speedometer') }}">
+              </use>
+            </svg> 
+            Dashboard
+            <span class="badge badge-sm bg-info ms-auto">
+              NEW
+            </span>
+          </a>
+        </li>
+      
+        <li class="nav-title">
+          Components
+        </li>
+        <li class="nav-group">
+          <a class="nav-link nav-group-toggle" href="#">
+            <svg class="nav-icon">
+              <use xlink:href="{{ asset('vendors/@coreui/icons/svg/free.svg#cil-puzzle') }}">
+              </use>
+            </svg> 
+            Base
+          </a>
+          <ul class="nav-group-items">
+            <li class="nav-item">
+              <a class="nav-link" href="base/breadcrumb.html">
+                <span class="nav-icon">
+                  </span> 
+                  Breadcrumb
+              </a>
+            </li>
+          </ul>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+            <svg class="nav-icon">
+              <use xlink:href="{{ asset('vendors/@coreui/icons/svg/free.svg#cil-speedometer') }}">
+              </use>
+            </svg> 
+            {{ __('Logout') }}
+          </a>
+          <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+            @csrf
+          </form>
+        </li>
+
+      </ul>
+    </div>
+
+    {{-- Sidebar end --}}
+
+    {{-- Main Contetn --}}
+    <div class="wrapper d-flex flex-column min-vh-100 bg-light" style="padding-left: 256px">
+    
+    {{-- Header --}}
+
+      <header class="header header-sticky mb-4">
+        <div class="container">
+        <button class="header-toggler px-md-0 me-md-3" type="button" onclick="coreui.Sidebar.getInstance(document.querySelector('#sidebar')).toggle()">
+        <svg class="icon icon-lg">
+        <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-menu"></use>
+        </svg>
+        </button><a class="header-brand d-md-none" href="#">
+        <svg width="118" height="46" alt="CoreUI Logo">
+        <use xlink:href="assets/brand/coreui.svg#full"></use>
+        </svg></a>
+        <ul class="header-nav ms-auto">
+        <li class="nav-item"><a class="nav-link" href="#">
+        <svg class="icon icon-lg">
+        <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-bell"></use>
+        </svg></a></li>
+        <li class="nav-item"><a class="nav-link" href="#">
+        <svg class="icon icon-lg">
+        <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-list-rich"></use>
+        </svg></a></li>
+        <li class="nav-item"><a class="nav-link" href="#">
+        <svg class="icon icon-lg">
+        <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-envelope-open"></use>
+        </svg></a></li>
+        </ul>
+        <ul class="header-nav ms-3">
+        <li class="nav-item dropdown"><a class="nav-link py-0" data-coreui-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+        <div class="avatar avatar-md"><img class="avatar-img" src="assets/img/avatars/8.jpg" alt="user@email.com"></div>
+        </a>
+        <div class="dropdown-menu dropdown-menu-end pt-0">
+        <div class="dropdown-header bg-light py-2">
+        <div class="fw-semibold">Account</div>
+        </div><a class="dropdown-item" href="#">
+        <svg class="icon me-2">
+        <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-bell"></use>
+        </svg> Updates<span class="badge badge-sm bg-info ms-2">42</span></a><a class="dropdown-item" href="#">
+        <svg class="icon me-2">
+        <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-envelope-open"></use>
+        </svg> Messages<span class="badge badge-sm bg-success ms-2">42</span></a><a class="dropdown-item" href="#">
+        <svg class="icon me-2">
+        <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-task"></use>
+        </svg> Tasks<span class="badge badge-sm bg-danger ms-2">42</span></a><a class="dropdown-item" href="#">
+        <svg class="icon me-2">
+        <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-comment-square"></use>
+        </svg> Comments<span class="badge badge-sm bg-warning ms-2">42</span></a>
+        <div class="dropdown-header bg-light py-2">
+        <div class="fw-semibold">Settings</div>
+        </div><a class="dropdown-item" href="#">
+        <svg class="icon me-2">
+        <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-user"></use>
+        </svg> Profile</a><a class="dropdown-item" href="#">
+        <svg class="icon me-2">
+        <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-settings"></use>
+        </svg> Settings</a><a class="dropdown-item" href="#">
+        <svg class="icon me-2">
+        <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-credit-card"></use>
+        </svg> Payments<span class="badge badge-sm bg-secondary ms-2">42</span></a><a class="dropdown-item" href="#">
+        <svg class="icon me-2">
+        <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-file"></use>
+        </svg> Projects<span class="badge badge-sm bg-primary ms-2">42</span></a>
+        <div class="dropdown-divider"></div><a class="dropdown-item" href="#">
+        <svg class="icon me-2">
+        <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-lock-locked"></use>
+        </svg> Lock Account</a><a class="dropdown-item" href="#">
+        <svg class="icon me-2">
+        <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-account-logout"></use>
+        </svg> Logout</a>
+        </div>
+        </li>
+        </ul>
+        </div>
+        <div class="header-divider"></div>
+      </header>
+
+      {{-- end of Header --}}
+
+      {{-- content --}}
+
+      <div class="body flex-grow-1 px-3">
+        <div class="container-lg">
+          @yield('content')
+        </div>
+      </div>
+
+      {{-- end of content --}}
+
+    </div>
+ <!-- Optional JavaScript -->
+ <!-- Popper.js first, then CoreUI JS -->
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.perfect-scrollbar/1.5.5/perfect-scrollbar.min.js" integrity="sha512-X41/A5OSxoi5uqtS6Krhqz8QyyD8E/ZbN7B4IaBSgqPLRbWVuXJXr9UwOujstj71SoVxh5vxgy7kmtd17xrJRw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+ <script src="https://unpkg.com/@popperjs/core@2"></script>
+ <script src="https://unpkg.com/@coreui/coreui/dist/js/coreui.min.js"></script>
+</body>
+</html>
+{{-- 
+<!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
@@ -80,4 +259,4 @@
         </main>
     </div>
 </body>
-</html>
+</html> --}}
